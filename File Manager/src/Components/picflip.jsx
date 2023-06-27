fbdeb8578e8a7fea952a918f7e3401c5cc0b1063
mhuzaifi0604 <huzzaifaasim@gmail.com>
2023-06-27 23:17:18 +0500

    Dashboard | Vendors | Files | chart

--- File Manager/src/Components/picflip.jsx
+++ File Manager/src/Components/picflip.jsx
@@ -31,11 +31,11 @@ function Picflip({imageurl, vendors, total, check}){
                         )}
 
                         {isContentVisible && (
-                            <div className={`${contentClasses} flex relative w-full h-full -mt-0 rounded-md bg-[#212134] bg-opacity-30 justify-center items-center border border-teal-500`} onClick={toggleContent}>
-                                <p className="m-2 p-2 text-teal-500 font-sans font-extrabold text-4xl absolute top-1 left-15">
+                            <div className={`${contentClasses} flex relative w-full h-full mt-0 rounded-md bg-[#212134] bg-opacity-30 justify-center items-center border border-teal-500`} onClick={toggleContent}>
+                                <p className=" text-teal-500 font-sans font-extrabold text-4xl absolute top-1 left-15">
                                     {check ? "Total Vendors" : "Total Files"}
                                 </p>
-                                <p className="m-2 p-2 text-teal-500 font-sans font-extrabold text-8xl absolute bottom-1 right-15">
+                                <p className=" text-teal-500 font-sans font-extrabold text-8xl absolute bottom-1 right-15">
                                     {check ? `${vendors}` : `${total}`}
                                 </p>
                             </div>
